0db33289096c1707414e8df4ac558f4767b7c0fc
hungbui <hung.bui@robustaeng.com>
2021-08-23 04:15:59 +0000

    add login, register, main ui, remove junk files

--- hackathon/core/app/Http/Controllers/AuthController.php
+++ hackathon/core/app/Http/Controllers/AuthController.php
@@ -89,7 +89,7 @@ class AuthController extends Controller
     {
         auth('api')->logout();
 
-        return response()->json(['message' => 'Successfully logged out']);
+        return response()->json(['status' => 'success']);
     }
 
     /**
@@ -112,7 +112,7 @@ class AuthController extends Controller
     protected function respondWithToken($token)
     {
         return response()->json([
-            'access_token' => $token,
+            'token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth('api')->factory()->getTTL() * 60
         ]);
